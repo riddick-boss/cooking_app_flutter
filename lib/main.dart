2bf999beef49e35ee37b25580a5ad76fb2b0e8df
Pawel Kremienowski <Kremienowski33@gmail.com>
2022-07-10 22:48:28 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:cooking_app_flutter/core/assets/string/app_strings.dart';
 import 'package:cooking_app_flutter/core/navigation/main_drawer/main_drawer.dart';
 import 'package:cooking_app_flutter/core/navigation/navigator_util.dart';
+import 'package:cooking_app_flutter/firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -11,8 +16,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         title: AppStrings.appName,
         navigatorKey: NavigatorUtil.mainAppNav,
         theme: ThemeData(
