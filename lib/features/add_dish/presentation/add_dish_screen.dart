e6ebc75ed8f65805136e31a19b228151bec69ffa
Pawel Kremienowski <kremienowski33@gmail.com>
2022-08-19 23:03:37 +0200

    cleanup

--- lib/features/add_dish/presentation/add_dish_screen.dart
+++ lib/features/add_dish/presentation/add_dish_screen.dart
@@ -5,8 +5,6 @@ import 'package:cooking_app_flutter/features/add_dish/presentation/add_dish_vm.d
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-//TODO: reformat
-
 class AddDishScreen extends StatefulWidget {
   const AddDishScreen({super.key});
 
@@ -42,85 +40,89 @@ class _AddDishScreenState extends State<AddDishScreen> {
     final dishName = _dishNameController.text;
     final category = _categoryController.text;
     final preparationTime = int.parse(_preparationTimeController.text);
-    await _viewModel.onCreateDishClicked(dishName: dishName, preparationTimeInMinutes: preparationTime, category: category);
+    await _viewModel.onCreateDishClicked(
+      dishName: dishName,
+      preparationTimeInMinutes: preparationTime,
+      category: category,
+    );
   }
 
   @override
   void dispose() {
-  //  TODO
+    //  TODO
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text("Add dish"),
-    ), // TODO
-    body: Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                // Expanded(
-                //   child: TextFormField(
-                  TextFormField(
-                    controller: _dishNameController,
-                    validator: (name) => null, // TODO
-                    decoration: InputDecoration(
-                      hintText: "Dish name", //TODO
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+        appBar: AppBar(
+          title: const Text("Add dish"),
+        ), // TODO
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // Expanded(
+                    //   child: TextFormField(
+                    TextFormField(
+                      controller: _dishNameController,
+                      validator: (name) => null, // TODO
+                      decoration: InputDecoration(
+                        hintText: "Dish name", //TODO
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
-                  ),
-                // ),
-                const SizedBox(height: 20),
-                // Expanded(
-                //   child: TextFormField(
-                  TextFormField(
-                    controller: _categoryController,
-                    validator: (name) => null, // TODO
-                    decoration: InputDecoration(
-                      hintText: "Category", //TODO
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                    // ),
+                    const SizedBox(height: 20),
+                    // Expanded(
+                    //   child: TextFormField(
+                    TextFormField(
+                      controller: _categoryController,
+                      validator: (name) => null, // TODO
+                      decoration: InputDecoration(
+                        hintText: "Category", //TODO
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
-                  ),
-                // ),
-                const SizedBox(height: 20),
-                // Expanded(
-                //   child: TextFormField(
-                  TextFormField(
-                    controller: _preparationTimeController,
-                    validator: (name) => null, // TODO
-                    decoration: InputDecoration(
-                      hintText: "Preparation time", //TODO
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                    // ),
+                    const SizedBox(height: 20),
+                    // Expanded(
+                    //   child: TextFormField(
+                    TextFormField(
+                      controller: _preparationTimeController,
+                      validator: (name) => null, // TODO
+                      decoration: InputDecoration(
+                        hintText: "Preparation time", //TODO
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
-                  ),
-                // ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                    onPressed: onCreateDishClicked,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
-                  ),
-                  child: const Text(
-                    "Create dish",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                    // ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: onCreateDishClicked,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+                      ),
+                      child: const Text(
+                        "Create dish",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
               Expanded(
                 child: Stack(
                   children: [
