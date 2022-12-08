c00a060f88db20be0bbd50e0eea1748dae95f717
Pawel Kremienowski <kremienowski33@gmail.com>
2022-12-08 22:50:33 +0100

    refactor: change sign up ui

--- lib/features/sign_up/presentation/sign_up_screen.dart
+++ lib/features/sign_up/presentation/sign_up_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cooking_app_flutter/di/cooking_app_injection.dart';
+import 'package:cooking_app_flutter/domain/assets/graphics/graphics.dart';
 import 'package:cooking_app_flutter/domain/assets/string/app_strings.dart';
 import 'package:cooking_app_flutter/domain/navigation/main_app_nav.dart';
 import 'package:cooking_app_flutter/domain/util/snack_bar.dart';
@@ -28,16 +29,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
       final email = _emailController.text.trim();
       final password = _passwordController.text.trim();
       await _viewModel.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-          firstName: firstName,
-          lastName: lastName,
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
       );
     }
   }
 
   void onAlreadyRegisteredClicked() {
-    MainAppNav.navigator.currentState?.pushReplacementNamed(MainAppNavDestinations.login.route);
+    MainAppNav.navigator.currentState
+        ?.pushReplacementNamed(MainAppNavDestinations.login.route);
   }
 
   @override
@@ -49,7 +51,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
     });
 
     _viewModel.onNavigateToDishesScreenStream.listen((event) {
-      MainAppNav.navigator.currentState?.pushNamedAndRemoveUntil(MainAppNavDestinations.main.route, (route) => false); // clear stack and go to dishes screen
+      MainAppNav.navigator.currentState?.pushNamedAndRemoveUntil(
+        MainAppNavDestinations.main.route,
+        (route) => false,
+      ); // clear stack and go to dishes screen
     });
   }
 
@@ -66,13 +71,23 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Graphics.loginSingUpBackground.path),
+              fit: BoxFit.cover,
+            ),
+          ),
           padding: const EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
                 AppStrings.signUpTitle,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: Colors.black,
+                ),
               ),
               const SizedBox(
                 height: 60,
@@ -86,10 +101,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         Expanded(
                           child: TextFormField(
                             controller: _firstNameController,
-                            validator: (name) => _viewModel.isFirstNameValid(firstName: name) ? null : AppStrings.signUpFirstNameTextFieldHint,
+                            validator: (name) =>
+                                _viewModel.isFirstNameValid(firstName: name)
+                                    ? null
+                                    : AppStrings.signUpFirstNameTextFieldHint,
                             decoration: InputDecoration(
                               hintText: AppStrings.signUpFirstNameTextFieldHint,
-                              prefixIcon: const Icon(Icons.person),
+                              prefixIcon: const Icon(
+                                Icons.person,
+                                color: Colors.black45,
+                              ),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10),
                               ),
@@ -102,10 +123,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         Expanded(
                           child: TextFormField(
                             controller: _lastNameController,
-                            validator: (name) => _viewModel.isLastNameValid(lastName: name) ? null : AppStrings.signUpLastNameTextFieldHint,
+                            validator: (name) =>
+                                _viewModel.isLastNameValid(lastName: name)
+                                    ? null
+                                    : AppStrings.signUpLastNameTextFieldHint,
                             decoration: InputDecoration(
                               hintText: AppStrings.signUpLastNameTextFieldHint,
-                              prefixIcon: const Icon(Icons.person),
+                              prefixIcon: const Icon(
+                                Icons.person,
+                                color: Colors.black45,
+                              ),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10),
                               ),
@@ -119,10 +146,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     ),
                     TextFormField(
                       controller: _emailController,
-                      validator: (email) => _viewModel.isEmailValid(email: email) ? null : AppStrings.signUpEnterValidEmailMessage,
+                      validator: (email) =>
+                          _viewModel.isEmailValid(email: email)
+                              ? null
+                              : AppStrings.signUpEnterValidEmailMessage,
                       decoration: InputDecoration(
                         hintText: AppStrings.signUpEmailTextFieldHint,
-                        prefixIcon: const Icon(Icons.email),
+                        prefixIcon: const Icon(
+                          Icons.email,
+                          color: Colors.black45,
+                        ),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -133,10 +166,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     ),
                     TextFormField(
                       controller: _passwordController,
-                      validator: (password) => _viewModel.isPasswordValid(password: password) ? null : AppStrings.signUpPasswordTextFieldHint,
+                      validator: (password) =>
+                          _viewModel.isPasswordValid(password: password)
+                              ? null
+                              : AppStrings.signUpPasswordTextFieldHint,
                       obscureText: true,
                       decoration: InputDecoration(
-                        prefixIcon: const Icon(Icons.lock),
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                          color: Colors.black45,
+                        ),
                         hintText: AppStrings.signUpPasswordTextFieldHint,
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
@@ -150,6 +189,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       onPressed: onSignUpClicked,
                       style: ElevatedButton.styleFrom(
                         padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+                        backgroundColor: Colors.purpleAccent,
                       ),
                       child: const Text(
                         AppStrings.signUpSignUpButton,
@@ -164,10 +204,26 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const Text(AppStrings.signUpAlreadyRegistered),
+                        const Text(
+                          AppStrings.signUpAlreadyRegistered,
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
                         TextButton(
+                          style: ButtonStyle(
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.transparent),
+                          ),
                           onPressed: onAlreadyRegisteredClicked,
-                          child: const Text(AppStrings.signUpAlreadyRegisteredButton,),
+                          child: const Text(
+                            AppStrings.signUpAlreadyRegisteredButton,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
                         ),
                       ],
                     )
